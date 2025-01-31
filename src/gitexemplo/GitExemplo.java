e01472bd707f7e32efe77e671c228bb8a0aa92b1
Julio Assis <jucasegundo2@gmail.com>
2025-01-31 06:10:14 -0300

    desabafo

--- src/gitexemplo/GitExemplo.java
+++ src/gitexemplo/GitExemplo.java
@@ -1,20 +1,12 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
- */
+
 package gitexemplo;
 
-/**
- *
- * @author julro
- */
+
 public class GitExemplo {
 
-    /**
-     * @param args the command line arguments
-     */
+
     public static void main(String[] args) {
-        // TODO code application logic here
+        System.out.println("vai tomar no cu nao consigo dormir alguem me ajuda eu nao quero mais sentir esse vazio eu quero ser alguem na vida");
     }
     
 }
